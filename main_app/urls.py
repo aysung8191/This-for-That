f9c14fc96a7a75b0ea0c739bfa4fda5623e63cdd
szaccagni <sarahzaccagni@gmail.com>
2023-02-13 16:15:39 -0500

    adds trade detail and delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,7 +9,10 @@ urlpatterns = [
     path('items/create/', views.ItemCreate.as_view(), name='item_create'),
     path('items/<int:pk>/update/', views.ItemUpdate.as_view(), name='item_update'),
     path('items/<int:pk>/delete/', views.ItemDelete.as_view(), name='item_delete'),
-    path('trades/create/<int:item_id>',views.TradeCreate.as_view(), name ="trade_create"),
+    path('trades/', views.TradeList.as_view(), name='trades_index' ),
+    path('trades/<int:pk>', views.TradeDetail.as_view(), name='trade_detail'),
+    path('trades/create/<int:item_id>',views.TradeCreate.as_view(), name ='trade_create'),
+    path('trades/<int:pk>/delete/', views.TradeDelete.as_view(), name='trade_delete'),
     path('accounts/signup/', views.signup, name='signup'),
     path('accounts/login/', views.Login.as_view()),
 ]
