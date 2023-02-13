d8fd56a1baae6c9add896b501a16664bd0410e7b
szaccagni <sarahzaccagni@gmail.com>
2023-02-13 12:09:23 -0500

    adds styling to registration pgs and nav bar

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.views.generic import ListView
 from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import UserCreationForm
+from .models import Item
+from .forms import UserSignUpForm, UserLoginForm
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'home.html')
+
+class ItemsList(ListView):
+  model = Item
 
 def signup(request):
   error_message = ''
   if request.method == 'POST':
     # This is how to create a 'user' form object
     # that includes the data from the browser
-    form = UserCreationForm(request.POST)
+    form = UserSignUpForm(request.POST)
     if form.is_valid():
       # This will add the user to the database
       user = form.save()
@@ -22,6 +28,9 @@ def signup(request):
     else:
       error_message = 'Invalid sign up - try again'
   # A bad POST or a GET request, so render signup.html with an empty form
-  form = UserCreationForm()
+  form = UserSignUpForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
+
+class Login(LoginView):
+  authentication_form = UserLoginForm
