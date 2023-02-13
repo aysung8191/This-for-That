1a5923110f2dcdbef51fd6633b55725128214919
Bermimby <bermaimbert@hotmail.com>
2023-02-13 13:47:36 -0500

    Item Status

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+ITEM_STATUS=(
+    ('1','Draft'),
+    ('2','Ready to Trade'),
+    ('3','Traded')
+)
+
 TRADE_STATUS=(
     ('1','Proposed'),
     ('2','Approved'),
@@ -13,6 +19,11 @@ class Item(models.Model):
     name=models.CharField(max_length=150)
     description=models.CharField(max_length=500)
     user=models.ForeignKey(User,on_delete=models.CASCADE)
+    status=models.CharField(
+        max_length=1,
+        choices=ITEM_STATUS,
+        default=ITEM_STATUS[0][0]
+    )
     
     def __str__(self):
         return self.name
@@ -23,6 +34,7 @@ class Item(models.Model):
 class Trade(models.Model):
     item_primary=models.ForeignKey(Item, related_name="trade_primary", on_delete=models.CASCADE)
     item_proposed=models.ForeignKey(Item, related_name="trade_proposed", on_delete=models.CASCADE)
+    comment=models.CharField(max_length=1000, blank=True)
     status=models.CharField(
         max_length=1,
         choices=TRADE_STATUS,
@@ -30,5 +42,7 @@ class Trade(models.Model):
     )
     def __str__(self):
         return f"trade {self.primary_item} for {self.proposed_item}"
+    
+
     
 
